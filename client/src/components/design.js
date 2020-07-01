7ac4f635f3deaaee38edce44580607234264777f
Maneesh <maneeshvijaykar@gmail.com>
2020-07-01 12:39:58 +0530

    Search..

--- client/src/components/design.js
+++ client/src/components/design.js
@@ -9,7 +9,7 @@ const Design = () => {
 
   const [isMobile, setisMobile] = useState(false)
 
-  const topics = ["Array", "Sorting", "Strings", "Linked List", "Stacks & Queues", "Trees", "Dynamic Programming", "Graphs", "Searching", "Recursion", "Binray Searc Tree", "Mathematical", "Bit-Manipulation", "Heaps", "Hashing", "BackTracking", "Tries", "Famous Algorithms"]
+  const topics = ["Array", "Sorting", "Strings", "Linked List", "Stacks & Queues", "Trees", "Dynamic Programming", "Graphs", "Searching", "Recursion", "Binray Search Tree", "Mathematical", "Bit-Manipulation", "Heaps", "Hashing", "BackTracking", "Tries", "Famous Algorithms"]
   return (
     <>
       <NavBar isMobile={isMobile} currUser={"Maneesh"} toggleFunc={() => {
